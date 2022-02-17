0bf5758d75c75d546a0866c235ab7a686ab83159
noah <n.dormann@tum.de>
2022-02-17 23:02:05 +0100

    added co2 sensor mh_z19

--- dht22_co2_exporter.py
+++ dht22_co2_exporter.py
@@ -10,6 +10,7 @@ import argparse
 from prometheus_client import Gauge, start_http_server
 
 import Adafruit_DHT
+import mh_z19
 
 # Create a metric to track time spent and requests made.
 dht22_temperature_celsius = Gauge(
@@ -19,23 +20,38 @@ dht22_temperature_fahrenheit = Gauge(
 dht22_humidity = Gauge(
     'dht22_humidity', 'Humidity in percents provided by dht sensor')
 
+mh_z19_co2 = Gauge(
+    'mh_z19_co2', 'CO2 in ppm provided by MH_Z19 sensor')
+mh_z19_raw_temperature_celsius = Gauge(
+    'mh_z19_raw_temperature_celsius', 'Raw (with offset) Temperature in celsius provided by MH_Z19 sensor')
+mh_z19_unnamed = Gauge(
+    'mh_z19_unnamed', 'Unnamed value provided by MH_Z19 sensor')
+
 SENSOR = Adafruit_DHT.DHT22
 
 
-def read_sensor(pin):
-    humidity, temperature = Adafruit_DHT.read_retry(SENSOR, pin)
+def read_sensors(pin):
+    humidity, dht22_temperature = Adafruit_DHT.read_retry(SENSOR, pin)
+    mh_z19_data = mh_z19.read_all()
+    co2 = mh_z19_data['co2']
+    mh_z19_raw_temperature = mh_z19_data['TT']
+    unnamed_value = mh_z19_data['UhUl']
 
-    if humidity is None or temperature is None:
+    if humidity is None or dht22_temperature is None:
         return
 
-    if humidity > 200 or temperature > 200:
+    if humidity > 200 or dht22_temperature > 200:
         return
 
     dht22_humidity.set('{0:0.1f}'.format(humidity))
     dht22_temperature_fahrenheit.set(
-        '{0:0.1f}'.format(9.0/5.0 * temperature + 32))
+        '{0:0.1f}'.format(9.0/5.0 * dht22_temperature + 32))
     dht22_temperature_celsius.set(
-        '{0:0.1f}'.format(temperature))
+        '{0:0.1f}'.format(dht22_temperature))
+
+    mh_z19_co2.set(f'{co2}')
+    mh_z19_raw_temperature_celsius.set(f'{mh_z19_raw_temperature}')
+    mh_z19_unnamed.set(f'{unnamed_value}')
 
 
 def main():
@@ -57,7 +73,7 @@ def main():
         start_http_server(args.port)
 
     while True:
-        read_sensor(pin=args.gpio)
+        read_sensors(pin=args.gpio)
         time.sleep(args.interval)
 
 
